a74c1ee13308c22cc7490cc852b44ae0e8ad737f
mjmc <matthieu.j.m.colin@gmail.com>
2020-11-29 16:37:41 +0100

    V9

--- squelPartie1/src/environmentStudy/Car.java
+++ squelPartie1/src/environmentStudy/Car.java
@@ -41,6 +41,8 @@ public class Car {
 			this.leftPosition = new Case(leftPosition.absc - 1, leftPosition.ord);
 		}
 	}
+
+
 	
 	/* Fourni : addToGraphics() permettant d'ajouter un element graphique correspondant a la voiture*/
 	private void addToGraphics() {
